ed91b42dd435146584dc56faf57d71ad1f23917d
Vignesh S <vignura@gmail.com>
2018-10-02 18:34:23 +0530

    Release 1V00

--- RFID/RFID.ino
+++ RFID/RFID.ino
@@ -1,78 +1,143 @@
-//RFID
+/***********************************************************************************************/
+/*
+ *  \file       : RFID.ino
+ *  \date       : 30-SEP-2018 
+ *  \author     : Vignesh S 
+ *  \email      : vignura@gmail.com
+ *  \copyright  : All Rights are Reserved 
+/*
+/***********************************************************************************************/
 #include <SPI.h>
 #include <MFRC522.h>
 #include <EEPROM.h>
 #include <LiquidCrystal.h>
 
-#define SS_PIN 10
-#define RST_PIN 9
-#define EARSE 2
-#define RELAY 1 //Relay pin
-#define MOTOR_ON_TIME_SEC   (10 * 60UL)
+// pins
+#define SS_PIN        10
+#define RST_PIN       9
+#define EARSE         2
+#define RELAY         1 
+
+#define MOTOR_ON_TIME_SEC           (15 * 60UL)
+#define MAX_UIDS_ALLOWED            0x02
+#define LONG_PRESS_DELAY_COUNT      0xF6  // 7.2 seconds delay
+
+// make sure both addresses are seperated by atleast 4 Bytes
+int tdyaddr = 0;
+int totaddr = 4;
+int iPressCount = 0;
+unsigned long ulTodayCount = 0;
+unsigned long ulTotalCount = 0;
+const int UIDStartAddr = 0x0C;
+MFRC522::Uid g_arrUID[MAX_UIDS_ALLOWED];
 
 // initialize the library with the numbers of the interface pins
 const int rs = 3, en = 4, d4 = 5, d5 = 6, d6 = 7, d7 = 8;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
-// make sure both addresses are seperated by atleast 4 Bytes
-int tdyaddr = 0;
-int totaddr = 4;
-unsigned long ulTodayCount = 0;
-unsigned long ulTotalCount = 0;
+// Create MFRC522 instance.
+MFRC522 mfrc522(SS_PIN, RST_PIN);
 
-byte value;
-MFRC522 mfrc522(SS_PIN, RST_PIN);   // Create MFRC522 instance.
-
+/***********************************************************************************************/
+/*! 
+* \fn         :: setup()
+* \author     :: Vignesh S
+* \date       :: 01-OCT-2018
+* \brief      :: This function performs the following initializations
+*                1) Sets the Relay pins as output and sets its state to low
+                 2) Initializes SPI
+                 3) Initializes MFRC522 RFID reader
+                 4) Initializes 16X2 LCD display
+                 5) Initializes today count and total count from EEPROM
+                 6) Loads the stored UIDs from EEPROM
+                 7) LCD displays is updated with counts
+* \param[in]  :: None
+* \param[out] :: None
+* \return     :: None
+*/
+/***********************************************************************************************/
 void setup() 
 {
+  // set the relay pin as output, set its state to low
   pinMode(RELAY, OUTPUT);
   digitalWrite(RELAY, LOW);
 
-  //Serial.begin(9600);   // Initiate a serial communication
-  SPI.begin();      // Initiate  SPI bus
-  mfrc522.PCD_Init();   // Initiate MFRC522
+  // Serial.begin(9600);
+  SPI.begin();
+  mfrc522.PCD_Init();
+  lcd.begin(16, 2);
 
   // Serial.println("Put your card to the reader...");
   // Serial.println();
-  lcd.begin(16, 2);
-
-  EEPROM.get(tdyaddr, ulTodayCount);
+  
   EEPROM.get(totaddr, ulTotalCount);
+  EEPROM.get(tdyaddr, ulTodayCount);
+  loadUID();
+  UpdateLCD();
 }
 
+/***********************************************************************************************/
+/*! 
+* \fn         :: loop()
+* \author     :: Vignesh S
+* \date       :: 01-OCT-2018
+* \brief      :: This functions is called repeatedly from a infinite for loop in main().
+*                It does the following functions
+*                1) When Erase button is pressed, today count is reset
+*                2) When Erase button is pressed for long time (ie > 7.2 Sec) total count
+*                   is cleared
+*                3) If a valid RFID is detected by the MFRC522 RFID reader, then relay is turned
+*                   on for a fixed time (ie 15 minutes) and the lcd updated is updated with 
+*                   today and total counts. After fixed time, relay is  turned off. 
+*                4) If a invalid RFID is detected, then "Access Denied" is displayed in lcd
+* \param[in]  :: None
+* \param[out] :: None
+* \return     :: None
+*/
+/***********************************************************************************************/
 void loop() 
-{
-  lcd.clear();
-  lcd.setCursor(1,0);
-  lcd.print("LOOKING...");
-  
+{ 
+  // reset today count
+  if(digitalRead(EARSE) == 0)
+  {
+    if(iPressCount == 0)
+    {
+      ulTodayCount = 0;
+      EEPROM.put(tdyaddr, ulTodayCount);
+      UpdateLCD();
+    }
+    
+    iPressCount++;
+  }
+  else
+  {
+    iPressCount = 0;
+  }
+
+  // reset the total count
+  if(iPressCount >= LONG_PRESS_DELAY_COUNT)
+  {
+    ulTotalCount = 0;
+    EEPROM.put(totaddr, ulTotalCount);
+    iPressCount = 0;
+    UpdateLCD();
+  }
+
   // Look for new cards
   if ( ! mfrc522.PICC_IsNewCardPresent()) 
   {
-    // return;
+    return;
   }
 
   // Select one of the cards
   if ( ! mfrc522.PICC_ReadCardSerial()) 
   {
-    // return;
+    return;
   }
+ 
+  //print_UID(&mfrc522);
   
-  //Show UID on serial monitor
-  // Serial.print("UID tag :");
-  String content= "";
-  byte letter;
-  
-  for (byte i = 0; i < mfrc522.uid.size; i++) 
-  {
-     // Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
-     // Serial.print(mfrc522.uid.uidByte[i], HEX);
-     content.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
-     content.concat(String(mfrc522.uid.uidByte[i], HEX));
-  }
-  content.toUpperCase();
-
-  if (content.substring(1) == "90 CC 92 A3"||content.substring(1) == "9C 50 B2 79") //change here the UID of the card/cards that you want to give access
+  if (IsUIDValid(&mfrc522.uid)) //change here the UID of the card/cards that you want to give access
   { 
     // increment the counts
     ulTodayCount++;
@@ -92,15 +157,22 @@ void loop()
     lcd.clear();
     lcd.setCursor(1,0);
     lcd.print("Access Denied");
-  }
-
-  if(digitalRead(EARSE) == 0)
-  {
-    ulTodayCount = 0;
-    EEPROM.put(tdyaddr, ulTodayCount);
+    delay(2000);
+    UpdateLCD();
   }
 } 
 
+/***********************************************************************************************/
+/*! 
+* \fn         :: UpdateLCD()
+* \author     :: Vignesh S
+* \date       :: 01-OCT-2018
+* \brief      :: This function updates the LCD display with today count and total count
+* \param[in]  :: None
+* \param[out] :: None
+* \return     :: None
+*/
+/***********************************************************************************************/
 void UpdateLCD()
 {
     // update Today count
@@ -109,7 +181,7 @@ void UpdateLCD()
     lcd.print("TDY CNT: ");
     lcd.setCursor(10,0);
     lcd.print(ulTodayCount);
-    
+
     // update Total count
     lcd.setCursor(0,1);
     lcd.print("TOT CNT: ");
@@ -117,12 +189,141 @@ void UpdateLCD()
     lcd.print(ulTotalCount);
 }
 
-void delay_sec(unsigned long ulTimeSec)
+/***********************************************************************************************/
+/*! 
+* \fn         :: loadUID()
+* \author     :: Vignesh S
+* \date       :: 01-OCT-2018
+* \brief      :: This function loads the stored UIDs from EEPROM into global structure 
+* \global     ::  
+* \param[in]  :: None
+* \param[out] :: None
+* \return     :: None
+*/
+/***********************************************************************************************/
+void loadUID()
 {
+  for(int i = 0; i < MAX_UIDS_ALLOWED; i++)
+  {
+    EEPROM.get(((i * sizeof(MFRC522::Uid)) + UIDStartAddr), g_arrUID[i]);
+    //print_UID(&g_arrUID[i]);
+    //lcd.setCursor(0,1);
+    //lcd.print(i + 1);
+    //Serial.println(" loaded"); 
+    //delay(2000);
+  }
+}
+
+/***********************************************************************************************/
+/*! 
+* \fn         :: delay_sec()
+* \author     :: Vignesh S
+* \date       :: 01-OCT-2018
+* \brief      :: This function provides delay in seconds
+* \param[in]  :: ulTimeSec
+* \param[out] :: None
+* \return     :: None
+*/
+/***********************************************************************************************/
+void delay_sec(unsigned long ulTimeSec)
+{ 
   unsigned long i = 0;
   
   for(i = 0; i < ulTimeSec; i++)
   {
     delay(1000);
   }
+}
+
+/***********************************************************************************************/
+/*! 
+* \fn         :: IsUIDValid()
+* \author     :: Vignesh S
+* \date       :: 01-OCT-2018
+* \brief      :: This function validates the passed uid by comparing it with global array
+*                (g_arrUID) and returns true if a match is found else false is returned
+* \param[in]  :: uid
+* \param[out] :: None
+* \return     :: true; false
+*/
+/***********************************************************************************************/
+bool IsUIDValid(MFRC522::Uid *uid)
+{
+  int i = 0;
+  int j = 0;
+  
+  for(i = 0; i < MAX_UIDS_ALLOWED; i++)
+  {
+    // validate size
+    if(UIDCmp(uid, &g_arrUID[i]))
+    {
+      return true;
+    }
+  }
+  
+  return false;
+} 
+
+/***********************************************************************************************/
+/*! 
+* \fn         :: print_UID()
+* \author     :: Vignesh S
+* \date       :: 01-OCT-2018
+* \brief      :: This function prints the uid into lcd display and serial port
+* \param[in]  :: uid
+* \param[out] :: None
+* \return     :: None
+*/
+/***********************************************************************************************/
+void print_UID(MFRC522::Uid *uid)
+{
+  String UID = "";
+  Serial.print("UID tag :");
+  for (byte i = 0; i < uid->size; i++) 
+  {
+     UID.concat(String(uid->uidByte[i] < 0x10 ? " 0" : " "));
+     UID.concat(String(uid->uidByte[i], HEX));
+  }
+
+  Serial.print(UID);
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print("UID:");
+  lcd.print(UID);
+}
+
+/***********************************************************************************************/
+/*! 
+* \fn         :: UIDCmp()
+* \author     :: Vignesh S
+* \date       :: 01-OCT-2018
+* \brief      :: This function compares two uids and returns true if they are equal else returns
+*                false
+* \param[in]  :: uid1
+* \param[in]  :: uid2
+* \param[out] :: None
+* \return     :: true;false
+*/
+/***********************************************************************************************/
+bool UIDCmp(MFRC522::Uid *uid1, MFRC522::Uid *uid2)
+{
+  int i = 0;
+  // validate size
+  if(uid1->size == uid2->size)
+  {
+    for(i = 0; i < uid1->size; i++)
+    {
+      if(uid1->uidByte[i] != uid2->uidByte[i])
+      {
+        break;
+      }
+    }
+
+    if(i == uid1->size)
+    {
+      return true;
+    }
+  }
+  
+  return false;
 }
--- RFIDProg/RFIDProg/RFIDProg.ino
+++ RFIDProg/RFIDProg/RFIDProg.ino
@@ -1,3 +1,12 @@
+/***********************************************************************************************/
+/*
+ *  \file       : RFIDProg.ino
+ *  \date       : 30-SEP-2018 
+ *  \author     : Vignesh S 
+ *  \email      : vignura@gmail.com
+ *  \copyright  : All Rights are Reserved 
+/*
+/***********************************************************************************************/
 #include <SPI.h>
 #include <MFRC522.h>
 #include <EEPROM.h>
@@ -7,61 +16,181 @@
 #define RST_PIN           9
 #define MAX_UIDS_ALLOWED  0x02
 
-const int UIDStartAddr = 0x8;
-const int UIDSizeBytes = 0x4;
-
 // initialize the library with the numbers of the interface pins
 const int rs = 3, en = 4, d4 = 5, d5 = 6, d6 = 7, d7 = 8;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
-byte value;
 MFRC522 mfrc522(SS_PIN, RST_PIN);   // Create MFRC522 instance.
+MFRC522::Uid uid = {0};
+const int UIDStartAddr = 0x0C;
 
+/***********************************************************************************************/
+/*! 
+* \fn         :: setup()
+* \author     :: Vignesh S
+* \date       :: 01-OCT-2018
+* \brief      :: This function performs the following initializations
+*                1) Initializes Serial port
+                 2) Initializes SPI
+                 3) Initializes MFRC522 RFID reader
+                 4) Initializes 16X2 LCD display
+* \param[in]  :: None
+* \param[out] :: None
+* \return     :: None
+*/
+/***********************************************************************************************/
 void setup() {
 
   Serial.begin(9600);
-  SPI.begin();      // Initiate  SPI bus
-  mfrc522.PCD_Init();   // Initiate MFRC522
+  SPI.begin();
+  mfrc522.PCD_Init();
 
   Serial.println("Put your card to the reader...");
   Serial.println();
   lcd.begin(16, 2);
-}
-
-void loop() {
 
   lcd.clear();
   lcd.setCursor(1,0);
   lcd.print("LOOKING...");
-  
+}
+
+/***********************************************************************************************/
+/*! 
+* \fn         :: loop()
+* \author     :: Vignesh S
+* \date       :: 01-OCT-2018
+* \brief      :: This functions is called repeatedly from a infinite for loop in main().
+*                It does the following functions
+*                1) If a RFID is detected by the MFRC522 RFID reader, it is stored to EEPROM
+*                   it can store MAX_UIDS_ALLOWED number of UIDs of RFIDs into EEPROM
+* \param[in]  :: None
+* \param[out] :: None
+* \return     :: None
+*/
+/***********************************************************************************************/
+void loop() {
+
+  int iStoreAddr = 0;
+  byte byUIDCount = 0;
+ 
   // Look for new cards
   if ( ! mfrc522.PICC_IsNewCardPresent()) 
   {
-    // return;
+    return;
   }
 
   // Select one of the cards
   if ( ! mfrc522.PICC_ReadCardSerial()) 
   {
-    // return;
+    return;
   }
 
   //Show UID on serial monitor
-  Serial.print("UID tag :");
-  String content= "";
-  byte letter;
-  
-  for (byte i = 0; i < mfrc522.uid.size; i++) 
-  {
-     Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
-     Serial.print(mfrc522.uid.uidByte[i], HEX);
-  }
-  content.toUpperCase();
+  print_UID(&mfrc522.uid);
+  delay(2000);
 
-  Serial.println(content);
-  Serial.println(content.substring(1));
-  Serial.println(content.substring(2));
-  Serial.println(mfrc522.uid.size);
+  iStoreAddr = (UIDStartAddr + (byUIDCount * sizeof(MFRC522::Uid)));
   
+  // store UID to EEPROM
+  EEPROM.put(iStoreAddr, mfrc522.uid);
   
+  // print the stored address
+  Serial.print("\tstored to EEPROM at ");
+  Serial.println(iStoreAddr, HEX);
+
+  // read back and verify
+  Serial.print("Read Back: ");
+  lcd.setCursor(0, 1);
+  lcd.print("Read Back: ");
+
+  EEPROM.get(iStoreAddr, uid);
+  
+  if(UIDCmp(&uid, &mfrc522.uid))
+  {
+    Serial.print(" OK");
+    lcd.setCursor(11, 1);
+    lcd.print(" OK");
+  }
+  else
+  {
+    Serial.print(" FAIL");
+    lcd.setCursor(11, 1);
+    lcd.print(" FAIL");
+  }
+  
+  byUIDCount++;
+  if(byUIDCount >=  MAX_UIDS_ALLOWED)
+  {
+    byUIDCount = 0;
+  }
+
+  delay(2000UL);
+  Serial.println("Put your card to the reader...");
+  lcd.clear();
+  lcd.setCursor(1,0);
+  lcd.print("LOOKING...");
+}
+
+/***********************************************************************************************/
+/*! 
+* \fn         :: print_UID()
+* \author     :: Vignesh S
+* \date       :: 01-OCT-2018
+* \brief      :: This function prints the uid into lcd display and serial port
+* \param[in]  :: uid
+* \param[out] :: None
+* \return     :: None
+*/
+/***********************************************************************************************/
+void print_UID(MFRC522::Uid *uid)
+{
+  String UID = "";
+  Serial.print("UID tag :");
+  for (byte i = 0; i < uid->size; i++) 
+  {
+     UID.concat(String(uid->uidByte[i] < 0x10 ? " 0" : " "));
+     UID.concat(String(uid->uidByte[i], HEX));
+  }
+
+  Serial.print(UID);
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print("UID:");
+  lcd.print(UID);
+}
+
+/***********************************************************************************************/
+/*! 
+* \fn         :: UIDCmp()
+* \author     :: Vignesh S
+* \date       :: 01-OCT-2018
+* \brief      :: This function compares two uids and returns true if they are equal else returns
+*                false
+* \param[in]  :: uid1
+* \param[in]  :: uid2
+* \param[out] :: None
+* \return     :: true;false
+*/
+/***********************************************************************************************/
+bool UIDCmp(MFRC522::Uid *uid1, MFRC522::Uid *uid2)
+{
+  int i = 0;
+  // validate size
+  if(uid1->size == uid2->size)
+  {
+    for(i = 0; i < uid1->size; i++)
+    {
+      if(uid1->uidByte[i] != uid2->uidByte[i])
+      {
+        break;
+      }
+    }
+
+    if(i == uid1->size)
+    {
+      return true;
+    }
+  }
+  
+  return false;
 }
