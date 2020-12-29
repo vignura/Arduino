5411e8d3aedbce09fba0d70c1410b30b2f676f0d
Vignesh S <vignura@gmail.com>
2020-12-29 22:08:37 +0530

    Serial IP based implementation under progress

--- libraries/serial_com/serial_com.cpp
+++ libraries/serial_com/serial_com.cpp
@@ -1,40 +1,124 @@
 #include "serial_com.h"
 
+static uint8_t dev_addr[2];
+static uint8_t bind_addr[2];
+static uint8_t is_bound;
+
 /***********************************************************************************************/
 /*! 
-* \fn         :: is_valid_packet()
+* \fn         :: set_dev_addr()
 * \author     :: Vignesh S
-* \date       :: 27-DEC-2020
-* \brief      :: This function validates the received packet and return true if it a valid 
-*                command else returns false.
-* \param[in]  :: packet, packet_size
-* \param[out] :: out_iCmdID
-* \return     :: true or false
+* \date       :: 29-DEC-2020
+* \brief      :: This function sets the address of the serial device. The address will be used
+*                for receving packets and sending packets
+* \param[in]  :: address
+* \param[out] :: None
+* \return     :: SERIAL_SUCCESS or error code 
 */
 /***********************************************************************************************/
-bool is_valid_packet(serial_packet *packet, uint8_t address[2])
+int8_t set_dev_addr(uint8_t address[2])
+{
+  if(address == NULL)
+  {
+    return SERIAL_ERR_NULL_PTR;
+  }
+  
+  dev_addr[0] = address[0];
+  dev_addr[1] = address[1];
+
+  return SERIAL_SUCCESS;
+}
+
+/***********************************************************************************************/
+/*! 
+* \fn         :: set_address()
+* \author     :: Vignesh S
+* \date       :: 29-DEC-2020
+* \brief      :: This function sets the address of the serial device. The address will be used
+*                for receving packets and sending packets
+* \param[in]  :: address
+* \param[out] :: None
+* \return     :: SERIAL_SUCCESS or error code 
+*/
+/***********************************************************************************************/
+int8_t set_bind_addr(uint8_t address[2])
+{
+  if(address == NULL)
+  {
+    return SERIAL_ERR_NULL_PTR;
+  }
+  
+  bind_addr[0] = address[0];
+  bind_addr[1] = address[1];
+  is_bound = SERIAL_DEVICE_BOUND;
+
+  return SERIAL_SUCCESS;
+}
+
+/***********************************************************************************************/
+/*! 
+* \fn         :: validate_packet()
+* \author     :: Vignesh S
+* \date       :: 27-DEC-2020
+* \brief      :: This function validates the received packet's header, checksum, and
+*                destination address.
+* \param[in]  :: packet, packet_size
+* \param[out] :: out_iCmdID
+* \return     :: SERIAL_SUCCESS or error code
+*/
+/***********************************************************************************************/
+int8_t validate_packet(serial_packet *packet)
 {
   uint8_t checksum = 0;
 
   // validate header
-  if((packet->header != SERIAL_CMD_HEADER) && (packet->header != SERIAL_RES_HEADER))
+  if(packet->header != SERIAL_HEADER)
   {
-    return false;
+    return SERIAL_ERR_INVALID_HEADER;
   }
   
   // validate checksum 
   if(compute_checksum(packet) != packet->checksum)
   {
-    return false;
+    return SERIAL_ERR_INVALID_CHKSUM;
   }
 
-  // validate address
-  if((packet->address[0] != address[0]) || (packet->address[1] != address[1]))
+  // validate destination address
+  if((packet->dst_addr[0] != dev_addr[0]) || (packet->dst_addr[1] != dev_addr[1]))
   {
-    return false;
+    return SERIAL_ERR_DST_ADDR;
   }
 
-  return true;
+  // validate data size
+  if(data_size > MAX_DATA_SIZE)
+  {
+    return SERIAL_ERR_INVALID_DATA_SIZE;
+  }
+
+  return SERIAL_SUCCESS;
+}
+
+/***********************************************************************************************/
+/*! 
+* \fn         :: validate_src_addr()
+* \author     :: Vignesh S
+* \date       :: 27-DEC-2020
+* \brief      :: This function validates the received packet's source address.
+* \param[in]  :: packet, src_addr
+* \param[out] :: None
+* \return     :: SERIAL_SUCCESS or error code
+*/
+/***********************************************************************************************/
+int8_t validate_src_addr(serial_packet *packet, uint8_t src_addr[2])
+{
+  // if device is bound to bind address, then we receive only form 
+  // that source address 
+  if((packet->src_addr[0] != src_addr[0]) || (packet->src_addr[1] != src_addr[1]))
+  {
+    return SERIAL_ERR_SRC_ADDR;
+  }
+  
+  return SERIAL_SUCCESS; 
 }
 
 /***********************************************************************************************/
@@ -51,18 +135,42 @@ bool is_valid_packet(serial_packet *packet, uint8_t address[2])
 uint8_t compute_checksum(serial_packet *packet)
 {
   uint8_t checksum = 0;
+  uint8_t *data = (uint8_t *)packet;
 
-  // compute checksum
-  checksum ^= packet->header;
-  checksum ^= packet->address[0];
-  checksum ^= packet->address[1];
-  checksum ^= packet->cmdID;
-
-  for (int i = 0; i < ((packet->data_size) - SERIAL_HEADER_SIZE); ++i)
+  for(int i = 0; i < (packet->data_size + SERIAL_MIN_PKT_SIZE); ++i)
   {
     /* code */
-    checksum ^= packet->data[i];
+    checksum ^= data[i];
   }
 
   return checksum;
+}
+
+/***********************************************************************************************/
+/*! 
+* \fn         :: frame_packet()
+* \author     :: Vignesh S
+* \date       :: 27-DEC-2020
+* \brief      :: This function frames a serial packet
+* \param[in]  :: packet, data_buf, data_size, dst_addr
+* \param[out] :: none
+* \return     :: checksum
+*/
+/***********************************************************************************************/
+void frame_packet(serial_packet* packet, uint8_t *data_buf, uint8_t data_size, uint8_t dst_addr[2])
+{
+  packet->header = SERIAL_HEADER;
+  packet->src_addr[0] = dev_addr[0]; 
+  packet->src_addr[1] = dev_addr[1];
+  packet->dst_addr[0] = dst_addr[0];
+  packet->dst_addr[1] = dst_addr[1];
+  packet->data_size = data_size;
+
+  // copy the data
+  for(int i = 0; i < data_size; i++)
+  {
+    packet->data[i] = data_buf[i];
+  }
+
+  packet->checksum = compute_checksum(packet);
 }
--- libraries/serial_com/serial_com.h
+++ libraries/serial_com/serial_com.h
@@ -9,29 +9,54 @@
 #define SERIAL_READ_DELAY_MS    2
 
 #define MAX_DATA_SIZE           16
-#define SERIAL_HEADER_SIZE      6
-#define SERIAL_CMD_HEADER       0xAA
-#define SERIAL_RES_HEADER       0xEE
+#define SERIAL_HEADER           0xAA
+/* sizeof header + src_addr + dst_addr + data_size + checksum */
+#define SERIAL_MIN_PKT_SIZE     7
+#define SERIAL_MAX_PKT_SIZE     (SERIAL_MIN_PKT_SIZE + MAX_DATA_SIZE)
+
+#define SERIAL_DEVICE_BOUND         1
+#define SERIAL_DEVICE_NOT_BOUND     0
+#define SERIAL_MAX_RECV_TIMEOUT_MS  (0xFFFF)
+
+#define SERIAL_SUCCESS              0
+#define SERIAL_FAILURE              -1
+
+#define SERIAL_ERR_BASE                   (-128)
+#define SERIAL_ERR_NULL_PTR               (SERIAL_ERR_BASE + 1)
+#define SERIAL_ERR_INVALID_HEADER         (SERIAL_ERR_BASE + 2)
+#define SERIAL_ERR_INVALID_CHKSUM         (SERIAL_ERR_BASE + 3)
+#define SERIAL_ERR_DST_ADDR               (SERIAL_ERR_BASE + 4)
+#define SERIAL_ERR_SRC_ADDR               (SERIAL_ERR_BASE + 5)
+#define SERIAL_ERR_INVALID_DATA_SIZE      (SERIAL_ERR_BASE + 6)
+#define SERIAL_ERR_INVALID_PKT_SIZE       (SERIAL_ERR_BASE + 7)
+#define SERIAL_ERR_RECV_TIMEOUT           (SERIAL_ERR_BASE + 8)
+#define SERIAL_ERR_DEV_NOT_BOUND          (SERIAL_ERR_BASE + 9)
 
 
 typedef struct serial_packet{
 
   uint8_t header;
-  uint8_t address[2];
-  uint8_t cmdID;
+  uint8_t src_addr[2];
+  uint8_t dst_addr[2];
   uint8_t data_size;
-  uint8_t checksum;
   uint8_t data[MAX_DATA_SIZE];
+  uint8_t checksum;
 
 }serial_packet;
 
 
 /***********************************************************************************************/
-bool is_valid_packet(serial_packet *packet, uint8_t address[2]);
+int8_t set_dev_addr(uint8_t address[2]);
+int8_t set_bind_addr(uint8_t address[2]);
+int8_t validate_packet(serial_packet *packet);
+int8_t validate_src_addr(serial_packet *packet, uint8_t src_addr[2]);
 uint8_t compute_checksum(serial_packet *packet);
+void frame_packet(serial_packet* packet, uint8_t *data_buf, uint8_t size);
 /***********************************************************************************************/
 
 /***********************************************************************************************/
+extern static uint8_t bind_addr[2];
+extern static uint8_t is_bound;
 /***********************************************************************************************/
 /*! 
 * \fn         :: RecvCmd()
@@ -76,26 +101,158 @@ int RecvCmd(T &SPort, char *pBuff, int iBuflen)
 * \fn         :: recv_packet()
 * \author     :: Vignesh S
 * \date       :: 27-DEC-2020
-* \brief      :: This function reads data from serial port into packet and returns the number of
-*                bytes read.
-* \param[in]  :: SPort, packet
-* \return     :: iIndex
+* \brief      :: This function receives a packet from serial bus within given timeout
+* \param[in]  :: SPort, packet, timeout_ms
+* \return     :: received packet size or error code
 */
 /***********************************************************************************************/
 template <typename T>
-int recv_packet(T &SPort, serial_packet *packet)
+int8_t recv_packet(T &SPort, serial_packet *packet, uint16_t timeout_ms)
 {
-  int iIndex = 0;
+  int8_t iIndex = 0;
+  uint16_t count = 0;
   char *buff = (char*)packet;
- 
-  while(iIndex < sizeof(serial_packet))
+
+  // read data from serial port
+  while(1)
   {
-    delay(SERIAL_READ_DELAY_MS);
-    
-    if(SPort.available())
+    while(iIndex < (buf_size + SERIAL_MIN_PKT_SIZE))
     {
-      buff[iIndex] = SPort.read();
-      iIndex++;
+      delay(SERIAL_READ_DELAY_MS);
+      
+      if(SPort.available())
+      {
+        buff[iIndex] = SPort.read();
+        iIndex++;
+      }
+      else
+      {
+        break;
+      }
+    }
+
+    if(iIndex == (buf_size + SERIAL_MIN_PKT_SIZE))
+    {
+      break;
+    }
+
+    if(count >= (timeout_ms / SERIAL_READ_DELAY_MS))
+    {
+      return SERIAL_ERR_RECV_TIMEOUT;
+    }
+
+    count++;
+  }
+
+  /* if the packet size is less than min packet size or greater than max packet size,
+     then it is malformed packet which we can discard. */
+  if((iIndex < SERIAL_MIN_PKT_SIZE) || (iIndex > SERIAL_MAX_PKT_SIZE))
+  {
+    return SERIAL_ERR_INVALID_PKT_SIZE;
+  }
+
+  /* In a properly formed packet, checksum will be the last byte that we receive, 
+  our serial_packet's size can vary from minimum of SERIAL_MIN_PKT_SIZE (ie, no data)
+  to SERIAL_MAX_PKT_SIZE (ie, full data). Hence if the received packet size (iIndex)
+  is less than SERIAL_MAX_PKT_SIZE, then weneed to copy the last byte to checksum
+  field of serial_packet. */
+
+  if(iIndex < SERIAL_MAX_PKT_SIZE)
+  {
+    packet->checksum = buff[iIndex];
+  }
+
+  /* validate the packet */
+  ret = validate_packet(&packet, bind_addr);
+  if(ret != SERIAL_SUCCESS)
+  {
+    return ret;
+  }
+
+  return iIndex;
+}
+
+/***********************************************************************************************/
+/*! 
+* \fn         :: recv()
+* \author     :: Vignesh S
+* \date       :: 27-DEC-2020
+* \brief      :: This function receives a packet from serial bus. If the device is bound to
+*                receive form particular address, then packets from only that source will be
+*                recived else, packets from all srouces will be received. use set_bind_addr 
+*                to bind to a source address.
+* \param[in]  :: SPort, data_buf, buf_size, src_addr, timeout_ms
+* \return     :: received packet size or error code
+*/
+/***********************************************************************************************/
+template <typename T>
+int8_t recv(T &SPort, uint8_t* data_buf, uint8_t buf_size, uint16_t timeout_ms)
+{
+  int8_t ret = 0;
+  serial_packet packet = {0};
+ 
+  // validate input parameters
+  if(data_buf == NULL)
+  {
+    return SERIAL_ERR_NULL_PTR;
+  }
+
+  if((buf_size > MAX_DATA_SIZE) || (buf_size == 0))
+  {
+    return SERIAL_ERR_INVALID_DATA_SIZE;
+  }
+
+  if(is_bound != SERIAL_DEVICE_BOUND)
+  {
+    return SERIAL_ERR_DEV_NOT_BOUND;
+  }
+
+  // receive the packet
+  ret = recv_packet(SPort, &packet, timeout_ms);
+  if(ret < SERIAL_SUCCESS)
+  {
+    return ret;
+  }
+  
+  // validate the bind address
+  ret = validate_src_addr(&packet, bind_addr);
+  if(ret != SERIAL_SUCCESS)
+  {
+    return ret;
+  }
+
+  return ret;
+}
+
+/***********************************************************************************************/
+/*! 
+* \fn         :: recv_no_timeout()
+* \author     :: Vignesh S
+* \date       :: 27-DEC-2020
+* \brief      :: It is recv with no timeout so, it will indefinitely wait for a packet
+*                and only return on receiving a packet.
+* \param[in]  :: SPort, data_buf, buf_size
+* \return     :: received packet size or error code
+*/
+/***********************************************************************************************/
+template <typename T>
+int8_t recv_no_timeout(T &SPort, uint8_t* data_buf, uint8_t buf_size)
+{
+  int8_t ret = 0;
+
+  while(1)
+  {
+    ret = recv(SPort, data_buf, buf_size, SERIAL_MAX_RECV_TIMEOUT_MS);
+    if(ret != (buf_size + SERIAL_MIN_PKT_SIZE))
+    {
+      if(ret == SERIAL_ERR_RECV_TIMEOUT)
+      {
+        // ignore timeout error
+      }
+      else
+      {
+        return ret;
+      }  
     }
     else
     {
@@ -103,7 +260,52 @@ int recv_packet(T &SPort, serial_packet *packet)
     }
   }
 
-  return iIndex;
+  return ret;
+}
+
+/***********************************************************************************************/
+/*! 
+* \fn         :: recvfrom()
+* \author     :: Vignesh S
+* \date       :: 27-DEC-2020
+* \brief      :: It receives packet only from the requested source address. Do not use
+*                set_bind_addr to bind to particular address while using this function
+* \param[in]  :: SPort, data_buf, buf_size
+* \return     :: received packet size or error code
+*/
+/***********************************************************************************************/
+template <typename T>
+int8_t recvfrom(T &SPort, uint8_t src_addr[2], uint8_t* data_buf, uint8_t buf_size, uint16_t timeout_ms)
+{
+  int8_t ret = 0;
+  serial_packet packet = {0};
+ 
+  // validate input parameters
+  if(data_buf == NULL)
+  {
+    return SERIAL_ERR_NULL_PTR;
+  }
+
+  if((buf_size > MAX_DATA_SIZE) || (buf_size == 0))
+  {
+    return SERIAL_ERR_INVALID_DATA_SIZE;
+  }
+
+  // receive the packet
+  ret = recv_packet(SPort, &packet, timeout_ms);
+  if(ret < SERIAL_SUCCESS)
+  {
+    return ret;
+  }
+  
+  // validate the bind address
+  ret = validate_src_addr(&packet, src_addr);
+  if(ret != SERIAL_SUCCESS)
+  {
+    return ret;
+  }
+
+  return ret;
 }
 
 /***********************************************************************************************/
@@ -113,17 +315,17 @@ int recv_packet(T &SPort, serial_packet *packet)
 * \date       :: 27-DEC-2020
 * \brief      :: This function sends data to serial port from packet and returns the number of
 *                bytes sent.
-* \param[in]  :: SPort, packet
-* \return     :: iIndex
+* \param[in]  :: SPort, data_buf, data_buf
+* \return     :: received packet size or error code
 */
 /***********************************************************************************************/
 template <typename T>
-int send_packet(T &SPort, serial_packet *packet)
+int8_t send_packet(T &SPort, serial_packet *packet)
 {
   int iIndex = 0;
   char *buff = (char*)packet;
 
-  for (iIndex = 0; iIndex < (SERIAL_HEADER_SIZE + packet->data_size); ++iIndex)
+  for (iIndex = 0; iIndex < (SERIAL_MIN_PKT_SIZE + packet->data_size); ++iIndex)
   {
     SPort.print(buff[iIndex]);
   }
@@ -133,30 +335,133 @@ int send_packet(T &SPort, serial_packet *packet)
 
 /***********************************************************************************************/
 /*! 
-* \fn         :: transmit_command()
+* \fn         :: send()
+* \author     :: Vignesh S
+* \date       :: 27-DEC-2020
+* \brief      :: This function sends data to serial port from packet and returns the number of
+*                bytes sent.
+* \param[in]  :: SPort, data_buf, data_size
+* \return     :: sent packet size or error code
+*/
+/***********************************************************************************************/
+int8_t send(T &SPort, uint8_t *data_buf, uint8_t data_size)
+{
+  serial_packet packet = {0};
+
+  // validate input parameters
+  if(data_buf == NULL)
+  {
+    return SERIAL_ERR_NULL_PTR;
+  }
+
+  if((buf_size > MAX_DATA_SIZE) || (buf_size == 0))
+  {
+    return SERIAL_ERR_INVALID_DATA_SIZE;
+  }
+
+  if(is_bound != SERIAL_DEVICE_BOUND)
+  {
+    return SERIAL_ERR_DEV_NOT_BOUND;
+  }
+
+  // frame packet before sending
+  frame_packet(&packet, data_buf, data_size, bind_addr);
+
+  return send_packet(SPort, packet);
+}
+
+/***********************************************************************************************/
+/*! 
+* \fn         :: sendto()
+* \author     :: Vignesh S
+* \date       :: 27-DEC-2020
+* \brief      :: This function sends data to serial port from packet and returns the number of
+*                bytes sent.
+* \param[in]  :: SPort, data_buf, data_size
+* \return     :: sent packet size or error code
+*/
+/***********************************************************************************************/
+int8_t sendto(T &SPort, uint8_t dst_addr[2], uint8_t *data_buf, uint8_t data_size)
+{
+  serial_packet packet = {0};
+
+  // validate input parameters
+  if(data_buf == NULL)
+  {
+    return SERIAL_ERR_NULL_PTR;
+  }
+
+  if((buf_size > MAX_DATA_SIZE) || (buf_size == 0))
+  {
+    return SERIAL_ERR_INVALID_DATA_SIZE;
+  }
+
+  // frame packet before sending
+  frame_packet(&packet, data_buf, data_size, dst_addr);
+
+  return send_packet(SPort, packet);
+}
+
+/***********************************************************************************************/
+/*! 
+* \fn         :: transmit()
 * \author     :: Vignesh S
 * \date       :: 27-DEC-2020
 * \brief      :: This function transmits a command through a serial and receives the response 
-* \param[in]  :: SPort, cmd_packet, res_packet
-* \return     :: iIndex
+* \param[in]  :: SPort, tx_data, tx_data_len, rx_data, rx_data_len, timeout_ms
+* \return     :: success or error code
 */
 /***********************************************************************************************/
 template <typename T>
-int transmit_command(T &SPort, serial_packet *cmd_packet, serial_packet *res_packet, int timeout_ms)
+int8_t transmit(T &SPort, uint8_t* tx_data, uint8_t tx_data_len, uint8_t* rx_data, uint8_t rx_data_len,  uint16_t timeout_ms)
 {
-  int iRecvBytes = 0;
-  int count = 0;
-
-  send_packet(SPort, cmd_packet);
-
-  while((iRecvBytes < SERIAL_HEADER_SIZE) && (count < (timeout_ms / SERIAL_READ_DELAY_MS)))
+  int8_t ret = 0;
+  
+  ret = send(SPort, tx_data, tx_data_len);
+  if(ret != (tx_data_len + SERIAL_MIN_PKT_SIZE))
   {
-    iRecvBytes = recv_packet(SPort, res_packet);
-    count++;
+   return ret; 
+  }
+  
+  ret = recv(SPort, rx_data, rx_data_len, timeout_ms);
+  if(ret != (rx_data_len + SERIAL_MIN_PKT_SIZE))
+  {
+    return ret;
   }
 
-  return iRecvBytes;
+  return SERIAL_SUCCESS;
 }
+
+/***********************************************************************************************/
+/*! 
+* \fn         :: transmit()
+* \author     :: Vignesh S
+* \date       :: 27-DEC-2020
+* \brief      :: This function transmits a command through a serial and receives the response 
+* \param[in]  :: SPort, dst_addr, tx_data, tx_data_len, rx_data, rx_data_len, timeout_ms
+* \return     :: success or error code
+*/
+/***********************************************************************************************/
+template <typename T>
+int8_t transmit_to(T &SPort, uint8_t dst_addr[2], uint8_t* tx_data, uint8_t tx_data_len, uint8_t* rx_data, uint8_t rx_data_len,  uint16_t timeout_ms)
+{
+  int8_t ret = 0;
+  
+  ret = sendto(SPort, dst_addr, tx_data, tx_data_len);
+  if(ret != (tx_data_len + SERIAL_MIN_PKT_SIZE))
+  {
+   return ret; 
+  }
+  
+  ret = recvfrom(SPort, dst_addr, rx_data, rx_data_len, timeout_ms);
+  if(ret != (rx_data_len + SERIAL_MIN_PKT_SIZE))
+  {
+    return ret;
+  }
+
+  return SERIAL_SUCCESS;
+}
+
 /***********************************************************************************************/
 
 #endif // _SERIAL_COMM_H_
