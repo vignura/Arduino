82a27864694dc2cf39e08bf1cd25c732c20e4804
Vignesh <vignesh_sridharan@comcast.com>
2022-02-21 16:07:36 +0530

    added debug_println

--- libraries/utility/utility.h
+++ libraries/utility/utility.h
@@ -5,5 +5,6 @@
 
 void PrintBytes(unsigned char *ucBuffer, int iBuflen);
 bool StrnCmp(char *pString1, char *pString2, int iLen);
+void debug_println(const char *format, ...);
 
 #endif // _ARDUINO_UTILITY_CPP_
